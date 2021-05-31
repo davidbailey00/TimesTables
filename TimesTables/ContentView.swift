af77cecf69e12602030bc1eff43d5290fef199df
David Bailey <davidbailey00@outlook.com>
2021-05-31 10:34:21 +0100

    initial commit

---
+++ TimesTables/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TimesTables
+//
+//  Created by David Bailey on 31/05/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TimesTables/TimesTablesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimesTablesApp.swift
+//  TimesTables
+//
+//  Created by David Bailey on 31/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct TimesTablesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

6ce1fd2c524b39210a10bb1cce1032846a76984c
David Bailey <davidbailey00@outlook.com>
2021-05-31 14:42:09 +0100

    Split settings form into own view

--- TimesTables/ContentView.swift
+++ TimesTables/ContentView.swift
@@ -22,65 +22,74 @@ struct Question {
 }
 
 struct ContentView: View {
+    @State private var questions: [Question]? = nil
+
+    var body: some View {
+        NavigationView {
+            if questions == nil {
+                SettingsForm(questions: $questions)
+            }
+        }
+    }
+}
+
+struct SettingsForm: View {
+    @Binding var questions: [Question]?
+
     @State private var timesTable = 2
     @State private var maxMultiplier = 10
     @State private var questionAmount = QuestionAmount.ten
     @State private var randomOrder = true
 
-    @State private var questions: [Question]? = nil
-    @State private var questionNumber = 0
-
     var body: some View {
-        NavigationView {
-            Form {
-                Section(header: Text("Game settings")) {
-                    Stepper(value: $timesTable, in: 2 ... 12) {
-                        Text("\(timesTable) times table")
-                    }
-
-                    Stepper(value: $maxMultiplier, in: 3 ... 12) {
-                        Text("Up to \(timesTable) × \(maxMultiplier)")
-                    }
-
-                    HStack {
-                        Text("Questions")
-                        Spacer()
-                        Picker("Questions", selection: $questionAmount) {
-                            ForEach(QuestionAmount.allCases, id: \.self) {
-                                Text($0.rawValue)
-                            }
-                        }
-                        .pickerStyle(SegmentedPickerStyle())
-                        .frame(maxWidth: 184)
-                    }
-
-                    if questionAmount == .all {
-                        Toggle("Random order", isOn: $randomOrder)
-                    }
+        Form {
+            Section(header: Text("Game settings")) {
+                Stepper(value: $timesTable, in: 2 ... 12) {
+                    Text("\(timesTable) times table")
                 }
 
-                Section {
-                    Button(action: {
-                        switch questionAmount {
-                        case .five:
-                            generateRandomQuestions(5)
-                        case .ten:
-                            generateRandomQuestions(10)
-                        case .twenty:
-                            generateRandomQuestions(20)
-                        case .all:
-                            generateAllQuestions()
+                Stepper(value: $maxMultiplier, in: 3 ... 12) {
+                    Text("Up to \(timesTable) × \(maxMultiplier)")
+                }
+
+                HStack {
+                    Text("Questions")
+                    Spacer()
+                    Picker("Questions", selection: $questionAmount) {
+                        ForEach(QuestionAmount.allCases, id: \.self) {
+                            Text($0.rawValue)
                         }
-                    }) {
-                        HStack {
-                            Image(systemName: "play.circle.fill")
-                            Text("Start game")
-                        }
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                    .frame(maxWidth: 184)
+                }
+
+                if questionAmount == .all {
+                    Toggle("Random order", isOn: $randomOrder)
+                }
+            }
+
+            Section {
+                Button(action: {
+                    switch questionAmount {
+                    case .five:
+                        generateRandomQuestions(5)
+                    case .ten:
+                        generateRandomQuestions(10)
+                    case .twenty:
+                        generateRandomQuestions(20)
+                    case .all:
+                        generateAllQuestions()
+                    }
+                }) {
+                    HStack {
+                        Image(systemName: "play.circle.fill")
+                        Text("Start game")
                     }
                 }
             }
-            .navigationTitle("Times Tables")
         }
+        .navigationTitle("Times Tables")
     }
 
     func generateRandomQuestions(_ amount: Int) {

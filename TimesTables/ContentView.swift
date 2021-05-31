07fd3e563b08ffe1e411a61c5a603277c3cf68e8
David Bailey <davidbailey00@outlook.com>
2021-05-31 12:00:47 +0100

    Create initial settings UI

--- TimesTables/ContentView.swift
+++ TimesTables/ContentView.swift
@@ -7,10 +7,53 @@
 
 import SwiftUI
 
+enum QuestionAmount: String, CaseIterable, Identifiable {
+    case five = "5"
+    case ten = "10"
+    case twenty = "20"
+    case all = "All"
+
+    var id: String { self.rawValue }
+}
+
 struct ContentView: View {
+    @State private var timesTable = 2
+    @State private var questionAmount = QuestionAmount.ten
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Form {
+                Section(header: Text("Game settings")) {
+                    Picker("Table", selection: $timesTable) {
+                        ForEach(2 ..< 13, id: \.self) {
+                            Text("\($0) times table")
+                        }
+                    }
+
+                    HStack(spacing: 40) {
+                        Text("Questions")
+                        Picker("Questions", selection: $questionAmount) {
+                            ForEach(QuestionAmount.allCases, id: \.self) { amount in
+                                Text(amount.rawValue)
+                            }
+                        }
+                        .pickerStyle(SegmentedPickerStyle())
+                    }
+                }
+
+                Section {
+                    Button(action: {
+                        // @TODO
+                    }) {
+                        HStack {
+                            Image(systemName: "play.circle.fill")
+                            Text("Start game")
+                        }
+                    }
+                }
+            }
+            .navigationTitle("Times Tables")
+        }
     }
 }
 
